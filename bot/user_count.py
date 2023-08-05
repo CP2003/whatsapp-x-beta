1d38f31fa942005719f8a144b235930d799153b3
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-06 01:57:20 +0530

    Update user_count.py

--- bot/user_count.py
+++ bot/user_count.py
@@ -21,7 +21,7 @@ async def count_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_count = len(interacted_users) - 1
 
     # Send the user count to the admin
-    await update.message.reply_text(f"Total user count: {user_count}")
+    await update.message.reply_text(f" `Total users  : {user_count}`")
 
 
 
