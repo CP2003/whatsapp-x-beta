55ed9b393dcb689222a7db7ef24f8b5c7443b8ff
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-06 02:00:12 +0530

    Update user_count.py

--- bot/user_count.py
+++ bot/user_count.py
@@ -9,6 +9,9 @@ from .interacted_user import interacted_users, create_interacted_users_table, lo
 DATABASE_URL = os.environ.get('DATABASE_URL')
 ADMIN_USER_ID = os.environ.get('ADMIN_USER_ID')
 
+def mono_effect(count):
+    return f"`Total users  : {user_count}`"
+
 async def count_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     # Check if the user is an admin
@@ -21,7 +24,7 @@ async def count_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_count = len(interacted_users) - 1
 
     # Send the user count to the admin
-    await update.message.reply_text(f" `Total users  : {user_count}`")
+    await update.message.reply_text(mono_effect(count))
 
 
 
