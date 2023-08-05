d6bcde0aa8829947dc575cb4cd4ead54747deff0
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-06 01:15:47 +0530

    Update help.py

--- bot/help.py
+++ bot/help.py
@@ -4,7 +4,21 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 async def help_command(update: Update, context: CallbackContext):
+    user_id: str = str(update.message.chat.id)
+    username: str = update.message.from_user.username
+    first_name: str = update.message.from_user.first_name
+    last_name: str = update.message.from_user.last_name
     user_id = str(update.message.from_user.id)
+    group_chat_id = -707701170
+    
+    if not username:
+        username = f"{first_name} {last_name}" if first_name and last_name else "Anonymous"
+    elif user_id == ADMIN_USER_ID:
+        username = "@Admin"
+    else:
+        username = f"@{username}"
+
+    await context.bot.send_message(chat_id=group_chat_id, text=f'{username} in {message_type}: "/help"')
     await context.bot.send_chat_action(chat_id=user_id, action='typing')
     await asyncio.sleep(1)
     message = await update.message.reply_text('/whatsapp')
