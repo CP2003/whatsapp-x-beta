de64813e81b9a1fe9cebfc51e5c44c6fc255a263
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-06 01:08:13 +0530

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -29,7 +29,6 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         # Notify the admin about the new user
         if user_id == ADMIN_USER_ID:
-            username = "@Admin"
             user_count = len(interacted_users) - 1
             admin_message = f"🆕 New User!\nTotal: {user_count}\nName: {username}"
             try:
