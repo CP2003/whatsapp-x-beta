4b5e804aa101ce2b175ecc767eb0723337812a34
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-09 22:52:52 +0530

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -19,6 +19,10 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if not username:
         username = f"{new_user_name} {last_name}" if first_name and last_name else "Anonymous"
+
+    if user_id in interacted_users:
+        interacted_users.add(user_id)
+        save_interacted_users()
     else:
         username = f"@{username}"
 
