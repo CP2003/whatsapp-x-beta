a30463a8427e23107f9212241e2bb0b444942b90
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 00:21:26 +0530

    Update handle_response.py

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -3,5 +3,14 @@
 def handle_response(text: str):
     processed = text.lower()
 
+    if 'whatsapp' in processed:
+    # Send reply message with inline buttons
+        keyboard = [
+        [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
+        [InlineKeyboardButton('Sam Mods', callback_data='sam')]
+         ]    
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        return 'Please select your Whatsapp:', reply_markup
+
     return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
 
