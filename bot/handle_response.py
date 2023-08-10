2fc6c29b79f6fc1426d553611eda2feb04176d11
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 08:00:54 +0530

    Update handle_response.py

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -1,16 +1,23 @@
-from telegram import InlineKeyboardButton , InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import time
+
+def print_letter_by_letter(update, text, delay=0.1):
+    for letter in text:
+        update.message.reply_text(letter, parse_mode='Markdown')
+        time.sleep(delay)
 
 def handle_response(text: str):
     processed = text.lower()
 
     if 'whatsapp' in processed:
-    # Send reply message with inline buttons
         keyboard = [
-        [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
-        [InlineKeyboardButton('Sam Mods', callback_data='sam')]
-         ]    
+            [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
+            [InlineKeyboardButton('Sam Mods', callback_data='sam')]
+        ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        return 'Please select your Whatsapp:', reply_markup
+        
+        reply_text = 'Please select your Whatsapp:'
+        return reply_text, reply_markup
 
-    return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
-
+    reply_text = f"\"{text}\" is not in my database. Try /help to get commands."
+    return reply_text, None
