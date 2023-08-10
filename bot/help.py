ae68b18317c89e00e8c4ba10e927e3f57668b4bb
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 10:00:20 +0530

    Update help.py

--- bot/help.py
+++ bot/help.py
@@ -27,7 +27,8 @@ async def help_command(update: Update, context: CallbackContext):
         await context.bot.send_message(chat_id=group_chat_id, text=f'{username} in {message_type}: "/help"')
         await context.bot.send_chat_action(chat_id=user_id, action='typing')
         await asyncio.sleep(1)
-        message = await update.message.reply_text("\n⭕️ /whatsapp - to get WhatsApp mod apks")
+        message = await update.message.reply_text("\n".join([f"⭕️ {cmd}" for cmd in commands[:1]]))
+
 
         for i in range(1, len(commands) + 1):
             await asyncio.sleep(0.1)
