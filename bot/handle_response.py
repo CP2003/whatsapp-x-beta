d346773e3f841e15e3b1bc289e2f0fdad18e9d1c
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 08:27:12 +0530

    Update handle_response.py

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -1,6 +1,6 @@
 from telegram import InlineKeyboardButton , InlineKeyboardMarkup
 
-def handle_response(text: str):
+async def handle_response(text: str):
     processed = text.lower()
 
     if 'whatsapp' in processed:
@@ -10,6 +10,7 @@ def handle_response(text: str):
         [InlineKeyboardButton('Sam Mods', callback_data='sam')]
          ]    
         reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Select a WhatsApp mod:', reply_markup=reply_markup)
         return 'Please select your Whatsapp:', reply_markup
 
     return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
