5c14005b6469ed89e35726fb30be100b9e431916
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 09:26:10 +0530

    Update handle_response.py

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -4,12 +4,18 @@ def handle_response(text: str):
     processed = text.lower()
 
     if 'whatsapp' in processed:
-    # Send reply message with inline buttons
-        keyboard = [
+        return senwa(query)
+
+    return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
+
+
+async def senwa(query):
+    await query.message.edit_text(text="Download Fouad Mod Whatsapp APK files - Fouad Mods")
+    keyboard = [
         [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
         [InlineKeyboardButton('Sam Mods', callback_data='sam')]
          ]    
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        return 'Please select your Whatsapp:', reply_markup
+    return 'Please select your Whatsapp:', reply_markup
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.message.edit_reply_markup(reply_markup=reply_markup)
 
-    return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
