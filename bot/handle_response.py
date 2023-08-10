ee76990e48c60b86d0058bb72a5ee1c89d9ebfcd
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 08:10:36 +0530

    Update handle_response.py

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -1,29 +1,15 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-import time
+from telegram import InlineKeyboardButton , InlineKeyboardMarkup
 
-def send_letter_by_letter(update, text, delay=0.1):
-    message = update.message.reply_text("")
-
-    for letter in text:
-        message.edit_text(message.text + letter)
-        time.sleep(delay)
-
-def handle_response(update, text: str):
+def handle_response(text: str):
     processed = text.lower()
 
     if 'whatsapp' in processed:
+    # Send reply message with inline buttons
         keyboard = [
-            [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
-            [InlineKeyboardButton('Sam Mods', callback_data='sam')]
-        ]
+        [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
+        [InlineKeyboardButton('Sam Mods', callback_data='sam')]
+         ]    
         reply_markup = InlineKeyboardMarkup(keyboard)
+        return 'Please select your Whatsapp:', reply_markup
 
-        reply_text = 'Please select your Whatsapp:'
-        return reply_text, reply_markup
-
-    reply_text = f"\"{text}\" is not in my database. Try /help to get commands."
-    return reply_text, None
-
-# Example usage (assuming you have the update object from the Telegram API)
-response_text, reply_markup = handle_response(update, "Hello, how can I help you?")
-send_letter_by_letter(update, response_text)
+    return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
