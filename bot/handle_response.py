c8378cdca63b1a348bc90ba0214b2f29a5b4a3d8
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 08:33:38 +0530

    Update handle_response.py

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -4,12 +4,22 @@ def handle_response(text: str):
     processed = text.lower()
 
     if 'whatsapp' in processed:
-    # Send reply message with inline buttons
-        keyboard = [
-        [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
-        [InlineKeyboardButton('Sam Mods', callback_data='sam')]
-         ]    
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        return 'Please select your Whatsapp:', reply_markup
+        whatsapp_opt()
 
     return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
+
+
+async def whatsapp_opt(update: Update):
+    user_id = str(update.message.from_user.id)
+
+    # Create an inline keyboard with the options
+    keyboard = [
+        [InlineKeyboardButton('Whatsapp', callback_data='fmmods_whatsapp')],
+        [InlineKeyboardButton('FM Whatsapp', callback_data='fmmods_fmwhatsapp')],
+        [InlineKeyboardButton('GB Whatsapp', callback_data='fmmods_gbwhatsapp')],
+        [InlineKeyboardButton('Yo Whatsapp', callback_data='fmmods_yowhatsapp')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Send the message to the user
+    await update.message.reply_text('Download Fouad Mod Whatsapp APK files:', reply_markup=reply_markup)
