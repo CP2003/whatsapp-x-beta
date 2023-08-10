61e4387572a68523cd7817f31dfd12699903f0be
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 08:09:31 +0530

    Update handle_response.py

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -1,12 +1,14 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import time
 
-def print_letter_by_letter(update, text, delay=0.1):
+def send_letter_by_letter(update, text, delay=0.1):
+    message = update.message.reply_text("")
+
     for letter in text:
-        update.message.reply_text(letter, parse_mode='Markdown')
+        message.edit_text(message.text + letter)
         time.sleep(delay)
 
-def handle_response(text: str):
+def handle_response(update, text: str):
     processed = text.lower()
 
     if 'whatsapp' in processed:
@@ -15,13 +17,13 @@ def handle_response(text: str):
             [InlineKeyboardButton('Sam Mods', callback_data='sam')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        
+
         reply_text = 'Please select your Whatsapp:'
         return reply_text, reply_markup
 
     reply_text = f"\"{text}\" is not in my database. Try /help to get commands."
     return reply_text, None
 
-# Example usage
-response_text, reply_markup = handle_response("Hello, how can I help you?")
-print_letter_by_letter(response_text)
+# Example usage (assuming you have the update object from the Telegram API)
+response_text, reply_markup = handle_response(update, "Hello, how can I help you?")
+send_letter_by_letter(update, response_text)
